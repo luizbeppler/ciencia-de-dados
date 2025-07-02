8e1d2653be3050f5bb991f24bcd3dae0a16d1548
luizbeppler <luizbeppler61@gmail.com>
2025-07-02 13:51:19 +0000

    aula06 medidas separatrises

--- aula05.py
+++ aula05.py
@@ -1,5 +1,5 @@
 import numpy as np
-import pandas as pd
+import aula05 as pd
 import matplotlib.pyplot as plt
 
 # Conjunto de dados exemplo
@@ -8,3 +8,19 @@ dados = [10, 20, 20, 30, 40, 50, 50, 50, 60, 70, 80, 90, 100]
 # Calcular quartis
 quartis = np.percentile(dados, [25, 50, 75])
 print(f'Quartis: Q1={quartis[0]}, Q2={quartis[1]}, Q3={quartis[2]}')
+
+# Conjunto de dados exemplo
+dados = [10, 20, 30, 40, 50, 50, 60, 70, 80, 90, 100]
+
+# Calcular quartis
+quartis = np.percentile(dados, [25, 50, 75])
+print(f'Quartis: Q1={quartis[0]}, Q2={quartis[1]}, Q3={quartis[2]}')
+
+# Calcular decis
+decis = np.percentile(dados, [10, 20, 30, 40, 50, 60, 70, 80, 90])
+print(f'Decis: {decis}')
+
+# Calcular percentis
+percentis = np.percentile(dados, [10, 25, 50, 75, 90])
+print(f'Percentis: {percentis}')
+
