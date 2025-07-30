4beb7f198694b8a909507b822d09bb7b2c67a7bd
luizbeppler <luizbeppler61@gmail.com>
2025-07-30 13:42:56 +0000

    aula05,03

--- aula04.py
+++ aula04.py
@@ -15,3 +15,19 @@ print(f'Média: {media}')
 # Calcular a moda
 moda = stats.mode(notas, keepdims=True)
 print(f'Moda: {moda.mode[0]}, Frequência: {moda.count[0]}')
+
+
+# Conjunto de dados exemplo
+dados = [10, 20, 20, 30, 40, 50, 50, 50, 60, 70, 80, 90, 100]
+
+# Calcular quartis
+quartis = np.percentile(dados, [25, 50, 75])
+print(f'Quartis: Q1={quartis[0]}, Q2={quartis[1]}, Q3={quartis[2]}')
+
+# Calcular decis
+decis = np.percentile(dados, [10, 20, 30, 40, 50, 60, 70, 80, 90])
+print(f'Decis: {decis}')
+
+# Calcular percentis
+percentis = np.percentile(dados, [10, 25, 50, 75, 90])
+print(f'Percentis: {percentis}')
